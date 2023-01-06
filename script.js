30f0159d278999772e69b366e8c31fc60607c50b
muazam-cmd <muazamshahid7@gmail.com>
2023-01-06 14:12:14 +0400

    Storing values in Variables and displaying

---
+++ script.js
@@ -0,0 +1,14 @@
+// Operations through console
+
+console.log(5 + 4 * 23);
+
+// Storing Values in variables and displaying
+
+let myFirstName = 'Muazam';
+let secondName = 'Mzm';
+
+let myAge = '25';
+let myProfession = 'Software Engineer';
+
+console.log(myProfession);
+
