f30ab864e85e561fdb7ffbffb36141649a568585
muazam-cmd <muazamshahid7@gmail.com>
2023-01-10 17:45:41 +0400

    Coding Challenge #3 , Objects, Object Method statements

--- fundamentals2/script.js
+++ fundamentals2/script.js
@@ -1,67 +1,65 @@
-//////////////////////////////////////////////////// Arrays Basic
-
-// Introduction to Arrays
-// const friend1 = 'Muazam';
-// const friend2 = 'Dullah';
-// const friend3 = 'mzm';
-// const friends = ['Muazam', 'Dullah', 'mzm'];
-// console.log(friends);
-// const y = new Array(1991, 1984, 2008, 2020);
-// console.log(friends[0]);
-// console.log(friends[2]);
-// console.log(friends.length);
-// console.log(friends[friends.length - 1]);
-// friends[2] = 'Jay';
-// console.log(friends);
-// // friends = ['Bob', 'Alice']
-// const firstName = 'Muazam';
-// const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
-// console.log(jonas);
-// console.log(jonas.length);
-// // Exercise
-// const calcAge = function (birthYeah) {
-//     return 2037 - birthYeah;
-// }
-// const years = [1990, 1967, 2002, 2010, 2018];
-// const age1 = calcAge(years[0]);
-// const age2 = calcAge(years[1]);
-// const age3 = calcAge(years[years.length - 1]);
-// console.log(age1, age2, age3);
-// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
-// console.log(ages);
-
-/////////////////////////////////////////////// Challenge
-
-// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
-// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
-// 20%.
+// Coding Challenge #3
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+// implement the calculations! Remember: BMI = mass / height ** 2 = mass
+// / (height * height) (mass in kg and height in meter)
 // Your tasks:
-// 1. Write a function 'calcTip' that takes any bill value as an input and returns
-// the corresponding tip, calculated based on the rules above (you can check out
-// the code from first tip calculator challenge if you need to). Use the function
-// type you like the most. Test the function using a bill value of 100
-// 2. And now let's use arrays! So create an array 'bills' containing the test data
-// below
-// 3. Create an array 'tips' containing the tip value for each bill, calculated from
-// the function you created before
-// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
-// Test data: 125, 555 and 44
-// Hint: Remember that an array needs a value in each position, and that value can
-// actually be the returned value of a function! So you can just call a function as array
-// values (so don't store the tip values in separate variables first, but right in the new
-// array)
+// 1. For each of them, create an object with properties for their full name, mass, and
+// height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+// method on both objects). Store the BMI value to a property, and also return it
+// from the method
+// 3. Log to the console who has the higher BMI, together with the full name and the
+// respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+// Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+// tall
 
-const bill = 100;
+const Muazam = {
+    firstName: 'Muazam',
+    lastName: 'Shahid',
+    MiddleName: 'Mzm',
+    age: 25,
+    BirthYear: 1997,
+    Job: 'Software Engineer',
+    Friends: ['Dullah', 'Mzm', 'Muazam',],
+    hasDrivingLicense: 'true',
 
-function CalcTip(bill) {
+    calcAge: function (BirthYear) {
+        return 2023 - BirthYear;
+    }
+};
 
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const MarkObj = {
+    fullName: 'Marks Miller',
+    mass: 78,
+    Height: 1.69,
+
+    calcBMIMark: function () {
+        this.BMI = this.mass / this.Height ** 2;
+        return this.BMI;
+    }
 }
 
-const bills = [125, 555, 44];
-const tips = [CalcTip(bills[0]), CalcTip(bills[1]), CalcTip(bills[2])];
-console.log(bills, tips)
+const JohnObj = {
+    fullName: 'John Smith',
+    mass: 92,
+    Height: 1.95,
 
-// const tip = bill <= 300 && bill >= 50 ? bill * (15 / 100) : bill * (20 / 100)
+    calcBMIJohn: function () {
+        this.BMI = this.mass / this.Height ** 2;
+        return this.BMI;
+    }
+}
 
-// console.log(`The Bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);
+
+MarkObj.calcBMIMark()
+console.log(`Mark BMI IS:`, MarkObj.BMI);
+
+JohnObj.calcBMIJohn()
+console.log(`John    BMI IS:`, JohnObj.BMI);
+
+
+if (MarkObj.BMI > JohnObj.BMI) {
+    console.log(`${MarkObj.fullName} BMI (${MarkObj.BMI}) is higher than ${JohnObj.fullName} BMI (${JohnObj.BMI}!)`);
+} else {
+    console.log(`John BMI ${JohnObj.BMI} is higher than Mark's Bmi ${MarkObj.BMI} !`);
+}
