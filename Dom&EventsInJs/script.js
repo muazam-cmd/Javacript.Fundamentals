9c13c7ea3d2402b0b173a818bfb16b5431ea061a
muazam-cmd <muazamshahid7@gmail.com>
2023-01-12 13:04:30 +0400

    implementing the stage 1 for  the game, initial step. no number.

--- Dom&EventsInJs/script.js
+++ Dom&EventsInJs/script.js
@@ -1,11 +1,21 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = document.querySelector('.guess').value;
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number!';
+    }
+
+});
