a8809613f4d1a9d395acad2883f12420062d4a46
muazam-cmd <muazamshahid7@gmail.com>
2023-01-16 13:37:37 +0400

    opening Modals and refactoring the code.

--- Models/script.js
+++ Models/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const Modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const OpenModal = function () {
+    console.log('button clicked')
+    Modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const closeModel = function () {
+    Modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', OpenModal);
+
+
+btnCloseModel.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
